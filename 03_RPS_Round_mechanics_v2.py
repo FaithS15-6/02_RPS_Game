eea53062a4f36ed07bea79406ecfb92d53ae461e
FaithS15-6 <78391318+FaithS15-6@users.noreply.github.com>
2021-04-08 14:40:20 +1200

    format

---
+++ 03_RPS_Round_mechanics_v2.py
@@ -0,0 +1,57 @@
+# Functions used to check input is valid
+
+
+def check_rounds():
+    while True:
+        response = input("How many rounds: ")
+
+        round_error = "Please type either <enter>  \ or an integer that is more than zero"
+        if response !="":
+            try:
+                response = int(response)
+
+                if response < 1:
+                    print(round_error)
+                    continue
+
+            except ValueError:
+                print(round_error)
+                continue
+
+        return response
+
+# Main Routine goes here...
+
+round_played = 0
+choose_instructions = "Please choose rock (r), paper (p), or scissors (s)"
+
+# ask user for number of round. <enter> for infinite mode
+rounds = check_rounds()
+
+end_game = "no"
+while end_game == "no":
+
+    # Rounds Heading
+    print()
+    if rounds == "":
+        heading = "Continues Mode: Round {}".format(rounds_played + 1)
+        print (heading)
+        choose = input("{} or 'xxx' to end: ".format(choose_instructions))
+        if choose == "xxx":
+            break
+
+    else:
+        heading = "Round {} of {}".format(round_played + 1, rounds)
+        print(heading)
+        choose = input(choose_instructions)
+        if rounds_played == rounds - 1:
+            end_game = "yes"
+
+
+    # rest of loop / game
+    print("You chose {}".format (choose))
+
+    round_played += 1
+
+print("Thank you for playing😊")
+
